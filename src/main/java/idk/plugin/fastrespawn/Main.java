12b9c3501efea80ecbb3973e8c4e1266685f75ac
PetteriM1 <petteri.malkavaara1@gmail.com>
2019-06-12 20:14:22 +0300

    Call player death event and add death messages

--- src/main/java/idk/plugin/fastrespawn/Main.java
+++ src/main/java/idk/plugin/fastrespawn/Main.java
@@ -1,14 +1,26 @@
 package idk.plugin.fastrespawn;
 
 import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.block.Block;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.entity.EntityLiving;
 import cn.nukkit.event.EventHandler;
 import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
+import cn.nukkit.event.entity.EntityDamageByBlockEvent;
+import cn.nukkit.event.entity.EntityDamageByEntityEvent;
 import cn.nukkit.event.entity.EntityDamageEvent;
+import cn.nukkit.event.player.PlayerDeathEvent;
 import cn.nukkit.item.Item;
+import cn.nukkit.lang.TranslationContainer;
 import cn.nukkit.math.NukkitRandom;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Main extends PluginBase implements Listener {
 
@@ -28,6 +40,108 @@ public class Main extends PluginBase implements Listener {
             if (!p.hasPermission("fastrespawn")) {
                 return;
             }
+
+            boolean dmsg = c.getBoolean("deathMessages");
+            String msg = "";
+            List<String> params = new ArrayList<>();
+
+            if (dmsg) {
+                params.add(p.getDisplayName());
+
+                switch (e.getCause()) {
+                    case ENTITY_ATTACK:
+                        if (e instanceof EntityDamageByEntityEvent) {
+                            Entity ent = ((EntityDamageByEntityEvent) e).getDamager();
+                            if (ent instanceof Player) {
+                                msg = "death.attack.player";
+                                params.add(((Player) ent).getDisplayName());
+                                break;
+                            } else if (ent instanceof EntityLiving) {
+                                msg = "death.attack.mob";
+                                params.add(!Objects.equals(ent.getNameTag(), "") ? ent.getNameTag() : ent.getName());
+                                break;
+                            } else {
+                                params.add("Unknown");
+                            }
+                        }
+                        break;
+                    case PROJECTILE:
+                        if (e instanceof EntityDamageByEntityEvent) {
+                            Entity ent = ((EntityDamageByEntityEvent) e).getDamager();
+                            if (ent instanceof Player) {
+                                msg = "death.attack.arrow";
+                                params.add(((Player) ent).getDisplayName());
+                            } else if (ent instanceof EntityLiving) {
+                                msg = "death.attack.arrow";
+                                params.add(!Objects.equals(ent.getNameTag(), "") ? ent.getNameTag() : ent.getName());
+                                break;
+                            } else {
+                                params.add("Unknown");
+                            }
+                        }
+                        break;
+                    case VOID:
+                        msg = "death.attack.outOfWorld";
+                        break;
+                    case FALL:
+                        if (e.getFinalDamage() > 2) {
+                            msg = "death.fell.accident.generic";
+                            break;
+                        }
+                        msg = "death.attack.fall";
+                        break;
+                    case SUFFOCATION:
+                        msg = "death.attack.inWall";
+                        break;
+                    case LAVA:
+                        msg = "death.attack.lava";
+                        break;
+                    case FIRE:
+                        msg = "death.attack.onFire";
+                        break;
+                    case FIRE_TICK:
+                        msg = "death.attack.inFire";
+                        break;
+                    case DROWNING:
+                        msg = "death.attack.drown";
+                        break;
+                    case CONTACT:
+                        if (e instanceof EntityDamageByBlockEvent) {
+                            if (((EntityDamageByBlockEvent) e).getDamager().getId() == Block.CACTUS) {
+                                msg = "death.attack.cactus";
+                            }
+                        }
+                        break;
+                    case BLOCK_EXPLOSION:
+                    case ENTITY_EXPLOSION:
+                        if (e instanceof EntityDamageByEntityEvent) {
+                            Entity ent = ((EntityDamageByEntityEvent) e).getDamager();
+                            if (ent instanceof Player) {
+                                msg = "death.attack.explosion.player";
+                                params.add(((Player) ent).getDisplayName());
+                            } else if (ent instanceof EntityLiving) {
+                                msg = "death.attack.explosion.player";
+                                params.add(!Objects.equals(ent.getNameTag(), "") ? ent.getNameTag() : ent.getName());
+                                break;
+                            }
+                        } else {
+                            msg = "death.attack.explosion";
+                        }
+                        break;
+                    case MAGIC:
+                        msg = "death.attack.magic";
+                        break;
+                    case HUNGER:
+                        msg = "death.attack.starve";
+                        break;
+                    default:
+                        msg = "death.attack.generic";
+                        break;
+                }
+            }
+
+            PlayerDeathEvent ev = new PlayerDeathEvent(p, p.getDrops(), new TranslationContainer(msg, params.toArray(new String[0])), p.getExperienceLevel());
+            getServer().getPluginManager().callEvent(ev);
 
             if (e.getFinalDamage() >= p.getHealth()) {
                 if (c.getBoolean("resetHealth")) {
@@ -67,6 +181,10 @@ public class Main extends PluginBase implements Listener {
                     p.setExperience(0, 0);
                 } else if (c.getBoolean("resetXp")) {
                     p.setExperience(0, 0);
+                }
+
+                if (dmsg) {
+                    getServer().broadcast(ev.getDeathMessage(), Server.BROADCAST_CHANNEL_USERS);
                 }
 
                 p.teleport(p.getSpawn());
